772293d183c541ace4ee18bde2fabf54f4670e86
Miyuue <miyuue@outlook.com>
2025-09-03 19:17:30 -0500

    Prepared RoadHazardCalc, UPCCollection, and BarcodeGenerator for use.
    Updated manifest for new activities.
    
    Implemented methods in MainActivity for buttons to navigate to proper activities.

--- app/src/main/java/com/miyuue/acctoolkit/BarcodeGenerator.java
+++ app/src/main/java/com/miyuue/acctoolkit/BarcodeGenerator.java
@@ -1,4 +1,24 @@
 package com.miyuue.acctoolkit;
 
-public class BarcodeGenerator {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BarcodeGenerator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_barcode_generator);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
